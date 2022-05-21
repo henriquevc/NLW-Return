950e92a2d60e755e24d6b8ded68873b8756b4499
Henrique <henriquevc93@gmail.com>
2022-05-21 13:05:56 -0300

    implementação e finalização do projeto mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,64 @@
+import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { theme } from './src/theme';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import Widget from './src/components/Widget';
+import * as SplashScreen from 'expo-splash-screen'
+import * as Font from 'expo-font'
+import { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false)
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          Inter_400Regular,
+          Inter_500Medium
+        })
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View 
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background
+      }}
+      onLayout={onLayoutRootView}
+    >
+      <StatusBar
+        style="light"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Widget />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
